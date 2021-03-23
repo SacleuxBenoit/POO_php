ee1276e097757b4ed7e4b3c75f2dcc64c6fd983f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-23 16:53:26 +0100

    create 2 methods damage && winXp :arrow_right: index.php

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ class Personnage{
     private $_localisation;
     private $_experience;
     private $_degats;
+
+    // class = commence par une majuscule
+    // attributs private = commence par un _
+    
+    public function damage(){
+        
+    }
+
+    public function winXp(){
+
+    }
 }
 
 ?>
