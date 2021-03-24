9418b6e17bc01cf9d3bd36af5d7fac793e4ef3a1
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-24 14:01:53 +0100

    add some test with the public function damage() :arrow_right: index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 class Personnage{
     private $_force = 20;
     private $_experience = 0;
-    private $_degats = 0;
+    private $_degats = 1;
 
     // class = commence par une majuscule
     // attributs private = commence par un _
 
-    public function damage($persoToHit){
-        $persoToHit->_degats += $this->_force;
+    public function damage(Personnage $persoToHit){
+      $tt =  $persoToHit->_degats += $this->_force;
+      echo $persoToHit->_degats;
     }
 
     public function winXp(){
@@ -22,4 +23,12 @@ $firstPerso = new Personnage; // Création d'un objet avec la classe Personnage
 $secondPerso = new Personnage;
 
 $firstPerso->damage($secondPerso);
+$firstPerso->winXp();
+?>
+
+<br>
+
+<?php
+$secondPerso->damage($firstPerso);
+$secondPerso->winXp();
 ?>
